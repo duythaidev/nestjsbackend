fda65a5ae4cc1bf69520894a044a00632cee752e
duythaidev <nguyenduythai17092005@gmail.com>
2025-05-08 17:59:28 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
+
+  await app.listen(PORT ?? 3000);
+  Logger.log(
+    ` Microservice ready to receive Redis messages in PORT - ${PORT}\n Environment - `,
+  );
+}
+bootstrap();
